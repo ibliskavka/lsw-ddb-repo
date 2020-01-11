0e4063b057e5753ae4fee9314d97fd16c8de5b72
Ivan Bliskavka <ibliskavka@live.com>
2020-01-11 11:07:50 -0500

    decorate prop name

--- Lsw.Ddb.Repo/BaseModel.cs
+++ Lsw.Ddb.Repo/BaseModel.cs
@@ -1,16 +1,20 @@
-﻿namespace Lsw.Ddb.Repo
+﻿using Newtonsoft.Json;
+
+namespace Lsw.Ddb.Repo
 {
     public abstract class BaseModel
     {
         /// <summary>
         /// Partition Key
         /// </summary>
-        public abstract string Pk { get; }
+        [JsonProperty(PropertyName = "pk")]
+        public string Pk { get; set; }
 
         /// <summary>
         /// Sort Key
         /// </summary>
-        public abstract string Sk { get; }
+        [JsonProperty(PropertyName = "sk")]
+        public string Sk { get; set; }
 
         /// <summary>
         /// Custom validation. Called before writing to db.
